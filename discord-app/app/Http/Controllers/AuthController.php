024c789dec7c3caa6f7a56ee6fda1fbfc91b333d
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-14 14:17:54 +0200

    feat: added login and logout functions to AuthController

--- discord-app/app/Http/Controllers/AuthController.php
+++ discord-app/app/Http/Controllers/AuthController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Player;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
@@ -58,4 +60,80 @@ class AuthController extends Controller
             );
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|string',
+                'password' => 'required|string',
+            ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $user = User::query()->where('email', $request['email'])->first();
+        // Validamos si el usuario existe
+        if (!$user) {
+            return response(
+                ["success" => false, "message" => "Email or password are invalid",],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+        // Validamos la contraseña
+        if (!Hash::check($request['password'], $user->password)) {
+            return response(["success" => true, "message" => "Email or password are invalid"], Response::HTTP_NOT_FOUND);
+        }
+
+        $token = $user->createToken('apiToken')->plainTextToken;
+        $res = [
+            "success" => true,
+            "message" => "User logged successfully",
+            "token" => $token
+        ];
+        return response()->json(
+            $res,
+            Response::HTTP_ACCEPTED
+        );
+    } catch (\Throwable $th) {
+        Log::error("Login error: " . $th->getMessage());
+        return response()->json(
+            [
+                "success" => false,
+                "message" => "Login error"
+            ],
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+    }
+    }
+    public function logout(Request $request)
+{  try {
+    $accessToken = $request->bearerToken();
+    // Get access token from database
+    $token = PersonalAccessToken::findToken($accessToken);
+    // Revoke token
+    $token->delete();
+
+    return response(
+        [
+            "success" => true,
+            "message" => "Logout successfully"
+        ],
+        Response::HTTP_OK
+    );
+} catch (\Throwable $th) {
+    Log::error("Logout error: " . $th->getMessage());
+
+    return response()->json(
+        [
+            "success" => false,
+            "message" => "Profile error"
+        ],
+        Response::HTTP_INTERNAL_SERVER_ERROR
+    );
 }
+}
+}
+
+
+
+
